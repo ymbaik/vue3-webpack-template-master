8ab082dff450958aecf959f467717faaa771dc32
ymbaik <ymbaik@empas.com>
2021-05-09 20:57:33 +0900

    Create vue3-webpack-template-master project

---
+++ src/components/Fruit.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>{{ name }}</li>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      dafault: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  h1 {
+    color: red !important;
+  }
+</style>
